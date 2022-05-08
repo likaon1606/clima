7b888517e42f3984ee5c3f1b1c091561c5608c29
Ariel <ariel.fuentesgarcia@gmail.com>
2022-05-08 15:18:44 -0500

    modifiqued

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 function App() {
   const [weather, setWeather] = useState({});
   const [temp, setTemp] = useState();
   const [tempData, setTempData] = useState();
 
-   const [isCelsius, setIsCelsious] = useState(false);
-  const success = pos => {
-    console.log(pos.coords)
+  const [isCelsius, setIsCelsious] = useState(false);
+  const success = (pos) => {
+    console.log(pos.coords);
     const latitude = pos.coords.latitude;
     const longitude = pos.coords.longitude;
-    
-      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=93e0e1e3107b0969d6cc37d2796393f8`)
+
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=93e0e1e3107b0969d6cc37d2796393f8`
+      )
       .then((res) => {
         setWeather(res.data);
         setTempData(res.data.main.temp);
         setTemp((res.data.main.temp - 273.15).toFixed());
-
       });
   };
-  
-  useEffect(() =>{
-  navigator.geolocation.getCurrentPosition(success);
-  },[]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success);
+  }, []);
   const convert = () => {
     let value = 0;
     if (isCelsius) {
@@ -36,29 +38,54 @@ function App() {
     setTemp(value);
   };
   return (
-    <div className="App">      
-      <div>    
-      <ul  className='contents'>
-      <h1>By <br/>Ariel <br/>Fuentes <br/>García</h1>
-        <li><b>Country: </b>{weather.sys?.country}</li>
-        <li><b>City: </b>{weather.main?.name}</li>
-        <li><b>Wind Speed: </b>{weather.wind?.speed}</li>
-        <li><b>Latitude: </b>{weather.coord?.lat}</li>
-        <li><b>Longitude: </b>{weather.coord?.lon}</li>
-        <li><b>Pressure: </b>{weather.main?.pressure} (hpa)</li>
-        <li><b>Units: </b>{temp}
-          {isCelsius ? "  °F" : " °C"}</li>
+    <div className="App">
+      <h1 className="owner">
+        By <br />
+        Ariel <br />
+        Fuentes <br />
+        García
+      </h1>
+      <div className="principal-content">
+        <ul className="contents">
+          <li>
+            <b>Country: </b>
+            {weather.sys?.country}
+          </li>
+          <li>
+            <b>City: </b>
+            {weather.main?.name}
+          </li>
+          <li>
+            <b>Wind Speed: </b>
+            {weather.wind?.speed}
+          </li>
+          <li>
+            <b>Latitude: </b>
+            {weather.coord?.lat}
+          </li>
+          <li>
+            <b>Longitude: </b>
+            {weather.coord?.lon}
+          </li>
+          <li>
+            <b>Pressure: </b>
+            {weather.main?.pressure} (hpa)
+          </li>
+          <li>
+            <b>Units: </b>
+            {temp}
+            {isCelsius ? "  °F" : " °C"}
+          </li>
         </ul>
 
         <div>
-        <button className='Button' onClick={convert}>
-          Convert to
-          <br/>
+          <button className="Button" onClick={convert}>
+            Convert to
+            <br />
             C°/F°
-        </button>
+          </button>
         </div>
-        </div>
-      
+      </div>
     </div>
   );
 }
